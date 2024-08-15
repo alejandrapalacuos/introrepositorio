6f6177ee2bbc5c68fa649cb529ace29877bbcdaf
alejandrapalacuos <145595611+alejandrapalacuos@users.noreply.github.com>
2024-08-15 10:16:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ st.header("Hola mundo")
 st.write("Diferente tipo de letra")
 image = Image.open("Imagen1.png")
 st.image(image, caption="Interfaces multimodales")
+
+texto = st.text_input("Escribe algo", "Este es mi texto")
+st.write("El texto escrito es: ", texto
