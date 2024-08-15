7439be06a5dfd5327627e8a2149630724db31c0c
alejandrapalacuos <145595611+alejandrapalacuos@users.noreply.github.com>
2024-08-15 10:09:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from PIL import Image
 st.title(" Mi primera página")
 st.header("Hola mundo")
 st.write("Diferente tipo de letra")
+image = Image.open("Imagen1.png")
+st.image(image, caption="Interfaces multimodales")
